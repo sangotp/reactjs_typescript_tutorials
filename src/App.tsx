8672141dbead793d7c12a79cd9326e4f14cf9ea4
sangitgreenwich <68804474+sangitgreenwich@users.noreply.github.com>
2023-02-13 11:29:22 +0700

    Basic Props In ReactJS + Typescript Project

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
 import './App.css';
 import { Greet } from './components/Greet';
+import { Person } from './components/Person';
+import { PersonList } from './components/PersonList';
 
 function App() {
+  const personName = {
+    first: 'Le',
+    last: 'Sang',
+  }
+
+  const nameList = [
+    {
+      first: 'Bruce',
+      last: 'Wayne',
+    },
+    {
+      first: 'Clark',
+      last: 'Kent',
+    },
+    {
+      first: 'Princess',
+      last: 'Diana',
+    }
+  ]
+
   return (
     <div className="App">
-      <Greet name='Sang' />
+      <Greet name='Sang' messageCount={20} isLoggedIn={false} />
+      <Person name={personName} />
+      <PersonList names={nameList} />
     </div>
   );
 }
